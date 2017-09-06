7daba0b01647c9ddabe963125b310591a8d0e973
DillonLecco <31684542+DillonLecco@users.noreply.github.com>
2017-09-06 10:34:12 +0300

    С++

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define TheBasis-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TheBasisURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
